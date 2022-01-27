b26837547c9060a2686409b8fb8350ae1ab1db85
Jacob Davidson <jayz34567@gmail.com>
2022-01-27 15:20:38 -0500

    Update GeoguessrBattleRoyaleCheat.js

--- GeoguessrBattleRoyaleCheat.js
+++ GeoguessrBattleRoyaleCheat.js
@@ -12,41 +12,9 @@
 // ==/UserScript==
 
 var locationInfo;
-
-function getTargetUrl() {
-	const raw = document.querySelectorAll("#__NEXT_DATA__")[0].text;
-	const json = JSON.parse(raw);
-	const rounds = json.props.pageProps.game.rounds;
-	console.log(rounds);
-	const currentRound = rounds[rounds.length - 1];
-
-	const targetUrl = "https://google.com/maps/place/" + currentRound.lat + "," + currentRound.lng;
-
-	return targetUrl;
-}
-
-function getLat(d) {
-	const json = JSON.parse(d);
-	const rounds = json.rounds;
-	const currentRound = rounds[rounds.length - 1];
-
-	return currentRound.lat;
-}
-
-function getLng(d) {
-	const json = JSON.parse(d);
-	const rounds = json.rounds;
-	const currentRound = rounds[rounds.length - 1];
-
-	return currentRound.lng;
-}
-
-function getRound(d) {
-	const json = JSON.parse(d);
-	const round = json.currentRoundNumber;
-
-	return round;
-}
+var lat;
+var lng;
+var currentRound;
 
 function getToken() {
 	const raw = document.querySelectorAll("#__NEXT_DATA__")[0].text;
@@ -62,7 +30,6 @@ function sleep(ms) {
 
 (function() {
 	'use strict';
-    var reverseGeocoder=new BDCReverseGeocode();
     var roundn = 0;
 		var guessr = function () {
 			let done = Array.from(document.getElementsByClassName("popup-view__header")).find(e => e.innerText.includes('qualified'));
@@ -73,51 +40,76 @@ function sleep(ms) {
 				function $(d) { return document.getElementById(d); }
 				roundn += 1;
 
-				GM_xmlhttpRequest({
+
+                GM_xmlhttpRequest({
 					method: "post",
 					url: "https://game-server.geoguessr.com/api/battle-royale/"+getToken()+"/guess",
 					headers: { "Content-type" : "application/json" },
 					data: "{\"countryCode\":\"in\",\"roundNumber\":"+roundn+"}",
 					onload: function(e) { //console.log(e)
-						console.log("Send india guess for round "+getRound(e.responseText));
-                        console.log(e.responseText);
-						/* Get the administrative location information using a set of known coordinates */
-						reverseGeocoder.getClientLocation({
-							latitude: getLat(e.responseText),
-							longitude: getLng(e.responseText),
-						},async function(result) {
 
-// 							console.log(getLat(e.responseText) + " " + getLng(e.responseText));
-//                             $.getJSON("https://api.bigdatacloud.net/data/reverse-geocode-client?latitude="+getLat(e.responseText)+"&longitude="+getLng(e.responseText)+"&localityLanguage=en",
-//                                 function(countryResults) {
-//                                 console.log(countryResults.countryName);
-//                             })
-                            GM_xmlhttpRequest({
-                                method: "get",
-                                url: "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude="+getLat(e.responseText)+"&longitude="+getLng(e.responseText)+"&localityLanguage=en",
-                                onload: function(response) {
-                                locationInfo = JSON.parse(response.responseText);
-                            }
-                        });
+                        const json = JSON.parse(e.responseText);
+                        currentRound = json.currentRoundNumber;
+                        const rounds = json.rounds;
+
+                        //console.log(json);
+                        //console.log(rounds);
+                        //console.log(currentRound);
+
+                        lat = (rounds[currentRound - 1].lat);
+                        lng = (rounds[currentRound - 1].lng);
+
+						console.log("Send india guess for round "+currentRound);
+                        console.log(json);
+
+
+
 
 // 							await sleep(3000);
-						});
 					}
 				});}
 		};
+
+
     var restarter = function () {
 			let but = Array.from(document.getElementsByClassName("button button--medium button--ghost")).find(e => e.textContent === 'Play again');
 		if(but){
 			but.click();
 		}
 	};
+
+
 	window.setInterval(guessr, 10000);
  	window.setInterval(restarter, 30000);
+
+
     document.onkeydown = evt => {
         evt = evt || window.event;
-        if(evt.shiftKey && evt.altKey && evt.keyCode == 70){
-            alert(locationInfo.countryName);
+        if(lat != null){
+
+        if(evt.shiftKey && evt.altKey && evt.keyCode == 72){
+            GM_xmlhttpRequest({
+					method: "post",
+					url: "https://game-server.geoguessr.com/api/battle-royale/"+getToken()+"/guess",
+					headers: { "Content-type" : "application/json" },
+					data: "{\"countryCode\":\""+locationInfo.results[0].components.country_code+"\",\"roundNumber\":"+currentRound+"}",
+            onload: function(e) {
+            }
+            });
+            //
         }
+
+        if(evt.shiftKey && evt.altKey && evt.keyCode == 71){
+                        GM_xmlhttpRequest({
+                                method: "get",
+                                url: "https://api.opencagedata.com/geocode/v1/json?q="+lat+","+lng+"&key=7d5b74fc2db041ccbe74ee3d2039d785",
+                                onload: function(response) {
+                                locationInfo = JSON.parse(response.responseText);
+                                    console.log(locationInfo);
+                                alert(locationInfo.results[0].components.country);
+                            }
+                        });
+        }}
     };
 })();
 
